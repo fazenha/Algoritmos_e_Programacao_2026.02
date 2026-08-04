399a705d7639b0af50edcc01ed1794b2611c85fa
fazenha <htpixs@gmail.com>
2026-08-04 17:42:25 +0000

    Commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Mensagem de teste
+        System.out.println("Olá! Seu ambiente Java online está funcionando!");
+        
+        // Operação matemática simples
+        int a = 10;
+        int b = 5;
+        int soma = a + b;
+        System.out.println("Teste de soma (10 + 5) = " + soma);
+    }
+}
