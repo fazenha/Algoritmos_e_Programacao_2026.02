e7456a490521d305b95fa7a9647655995340ab01
Flavio <htpixs@gmail.com>
2026-08-06 20:50:27 -0300

    ok

--- Exemplo1.java
+++ Exemplo1.java
@@ -3,13 +3,15 @@ import java.util.Scanner;
 public class Exemplo1 {
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite:");
-        double n1 = sc.nextDouble();
+        Scanner sc = new Scanner(System.in); // Objeto para leitura do teclado
+        
+        System.out.println("Digite:"); // Mensagem em tela 
+        
+        double n1 = sc.nextDouble(); // Atribui o valor digitado
         double n2 = sc.nextDouble();
         double n3 = sc.nextDouble();
         double media = (n1+n2+n3)/3;
         
-        System.out.println("Media = "+media);
+        System.out.println("Media = "+media); // Exibe mensagem
     }
 }
