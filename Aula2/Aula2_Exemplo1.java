6bc697d67f737f716f8762cfe905ebd040edfda2
Flavio <htpixs@gmail.com>
2026-08-15 19:06:09 -0300

    Exemplo 1

---
+++ Aula2/Aula2_Exemplo1.java
@@ -0,0 +1,17 @@
+package Aula2;
+
+public class Aula2_Exemplo1 {
+	
+	public static void main(String[] args) {
+	
+		int idade = 27;
+		double peso = 54.90;
+		String nome = "Ana";
+
+		// erro pego na COMPILACAO:
+		// idade = "vinte";
+		
+		
+		System.out.printf("Olá,meu nome é " + nome + "%nEu tenho " + idade + " anos%n" +"Atualmente, eu tenho " + peso + "kg%n");
+	}
+}
